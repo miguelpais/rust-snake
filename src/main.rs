3def9d5ef29f75c675a2cb30510086c4a49275b1
Miguel Pais <miguel.pais@sysdig.com>
2023-03-24 10:36:14 +0100

    With a bunch of constants for improved readibility

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,12 @@ use crate::snake::{screen::Screen, input};
 
 use crossterm::terminal::{disable_raw_mode};
 
+const SCREEN_HEIGTH:usize = 40;
+const SCREEN_WIDTH:usize = 80;
+const FRAMES_PER_SECOND: u64 = 20;
+const UPDATE_EVERY_N_FRAMES: u8 = 5;
+const INPUT_CAPTURING_WINDOW_MS: u64 = 3;
+
 fn main() {
     let counter = Arc::new(Mutex::new(0));
 
@@ -14,11 +20,11 @@ fn main() {
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
-        input::input_loop(tx, lock_input_loop, 3);
+        input::input_loop(tx, lock_input_loop, INPUT_CAPTURING_WINDOW_MS);
     });
 
-    let mut screen = Screen::new(40, 80);
+    let mut screen = Screen::new(SCREEN_HEIGTH, SCREEN_WIDTH);
 
-    screen.main_loop(20, 5, rx, lock_main_loop);
+    screen.main_loop(FRAMES_PER_SECOND, UPDATE_EVERY_N_FRAMES, rx, lock_main_loop);
     disable_raw_mode();
 }
